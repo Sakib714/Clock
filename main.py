e54487c180706c16f2d385e4e343ca2fcfefde00
Sakib714 <sakib39.official@gmail.com>
2021-07-29 23:56:09 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Libraries
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+# Creating UI
+root = Tk()
+root.title('Sakib\'s Clock')
+
+
+# Main Function
+def time():
+    string = strftime('%H:%M:%S  %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+
+# Font, Background, Foreground
+label = Label(root, font=('ds-digital', 80), background='#0d0d0d', foreground='pink')
+label.pack(anchor='center')
+
+
+# Calling functions
+
+time()
+mainloop()
